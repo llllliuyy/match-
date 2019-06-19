74faa775b1c8a0e293293440bcbab2e82517cefa
liuxm <1971875450@qq.com>
2019-06-19 21:32:46 +0800

    bisai

--- match ++/main.cpp
+++ match ++/main.cpp
@@ -7,9 +7,43 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <fstream>
+using namespace std;
+int main()
+{
+    ofstream lxmcin("/Users/s20181106116/Desktop/lxmcin.txt");
+    if (lxmcin.is_open())
+    {
+        lxmcin<< "This is a line.\n";
+        lxmcin<< "This is another line.\n";
+        lxmcin.close();
+    }
     return 0;
 }
+void print(int arr[], int n)
+{
+    for(int j= 0; j<n; j++)
+    {
+        cout<<arr[j] <<"  ";
+    }
+    cout<<endl;
+}
+
+void BubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+
+
